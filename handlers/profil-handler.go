b2203a03585870a3e90da923f1aa43520e65277f
Mathis-Pain <mathis.pain76@gmail.com>
2025-10-20 17:39:01 +0200

    map and circle front ok

--- handlers/profil-handler.go
+++ handlers/profil-handler.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"RideUP/models"
+	"RideUP/sessions"
 	"RideUP/utils"
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,7 +18,33 @@ var ProfilHtml = template.Must(template.ParseFiles(
 ))
 
 func ProfilHandler(w http.ResponseWriter, r *http.Request) {
-	if err := ProfilHtml.Execute(w, nil); err != nil {
+
+	// Récupère l'utilisateur connecté
+	session, err := sessions.GetSessionFromRequest(r)
+	if err != nil {
+		log.Printf("Erreur pas d'utilisateur connecté")
+		http.Redirect(w, r, "/Connect", http.StatusSeeOther)
+		return
+	}
+
+	db, err := sql.Open("sqlite3", "./data/RideUp.db")
+	if err != nil {
+		utils.InternalServError(w)
+		return
+	}
+	defer db.Close()
+
+	// Coordonnées par défaut ou de l'utilisateur
+	var data models.MapData
+	err = db.QueryRow(`SELECT latitude, longitude FROM users WHERE id = ?`, session.UserID).
+		Scan(&data.Latitude, &data.Longitude)
+	if err != nil {
+		log.Println("Erreur récupération coordonnées:", err)
+		data.Latitude = 48.8566
+		data.Longitude = 2.3522
+	}
+
+	if err := ProfilHtml.Execute(w, data); err != nil {
 		log.Printf("Erreur lors de l'exécution du template rideup.html: %v", err)
 		utils.InternalServError(w)
 	}
