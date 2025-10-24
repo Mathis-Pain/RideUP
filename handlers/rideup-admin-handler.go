4a1b3ce7c1724d73eda9c34bff095615d58e925e
Mathis-Pain <mathis.pain76@gmail.com>
2025-10-24 14:23:47 +0200

    admin page

--- handlers/rideup-admin-handler.go
+++ handlers/rideup-admin-handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"RideUP/models"
 	"RideUP/sessions"
 	"RideUP/utils"
 	"RideUP/utils/getdata"
@@ -41,8 +42,73 @@ func RideUpAdminHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/RideUp", http.StatusSeeOther)
 		return
 	}
+	allRows, err := db.Query(`
+	SELECT e.id, e.title, e.description, e.created_by, u.username, e.created_at,
+	       e.latitude, e.longitude, e.address, e.start_datetime, e.end_datetime, e.participants
+	FROM events e
+	JOIN users u ON e.created_by = u.id
+	ORDER BY e.start_datetime ASC`)
+	if err != nil {
+		log.Println("Erreur SELECT availableEvents:", err)
+		utils.InternalServError(w)
+		return
+	}
+	defer allRows.Close()
 
-	err = RideUpAdminHtml.Execute(w, nil)
+	var availableEvents []models.Event
+	for allRows.Next() {
+		var e models.Event
+		if err := allRows.Scan(
+			&e.ID,
+			&e.Title,
+			&e.Description,
+			&e.CreatedBy,
+			&e.CreatorName,
+			&e.CreatedAt,
+			&e.Latitude,
+			&e.Longitude,
+			&e.Address,
+			&e.StartDatetime,
+			&e.EndDatetime,
+			&e.Participants,
+		); err != nil {
+			log.Println("Erreur Scan availableEvents:", err)
+			continue
+		}
+		availableEvents = append(availableEvents, e)
+	}
+	var dashBoard models.AdminDashBoard
+
+	// Compter le nombre total d’utilisateurs
+	err = db.QueryRow(`SELECT COUNT(*) FROM users`).Scan(&dashBoard.NumberUsers)
+	if err != nil {
+		log.Println("Erreur COUNT users:", err)
+		return
+	}
+
+	// Compter le nombre d’utilisateurs connectés
+	err = db.QueryRow(`SELECT COUNT(*) FROM sessions `).Scan(&dashBoard.NumberUserConnected)
+	if err != nil {
+		log.Println("Erreur COUNT connected users:", err)
+		return
+	}
+
+	// Compter le nombre d’événements
+	err = db.QueryRow(`SELECT COUNT(*) FROM events`).Scan(&dashBoard.NumberEvent)
+	if err != nil {
+		log.Println("Erreur COUNT events:", err)
+		return
+	}
+
+	data := struct {
+		AvailableEvents []models.Event
+		DashBoard       models.AdminDashBoard
+	}{
+		AvailableEvents: availableEvents,
+		DashBoard:       dashBoard,
+	}
+
+	err = RideUpAdminHtml.Execute(w, data)
 	if err != nil {
 		log.Printf("Erreur lors de l'exécution du template RideUpAdmin: %v", err)
 		utils.NotFoundHandler(w)
